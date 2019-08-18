84029fdd9e088df9e4ffbe03aaae21f323a311e1
root <root@PT-LT0357.marvell.com>
2019-08-18 08:23:41 +0300

    added migrations

--- config.py
+++ config.py
@@ -5,7 +5,9 @@ class BaseConfig(object):
 	DEBUG = False
 	SECRET_KEY = '\xa9\xde\xfd\x95\xe8L\x08g)F\xf0\xf8\x1e\xa1\xe9kx\x0e!\x89\x9d\x16\xb5\xfa'
 	# export DATABASE_URL="sqlite:///posts.db"
+	# export DATABASE_URL="postgresql:///discover_flask_dev"
 	SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+	print(SQLALCHEMY_DATABASE_URI)
 	SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 class DevelopmentConfig(BaseConfig):
