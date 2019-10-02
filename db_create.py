66e329e08e38889f45f6a6aeb1108804328ce48a
root <root@PT-LT0357.marvell.com>
2019-10-02 11:03:02 +0300

    refactored into blueprints again

--- db_create.py
+++ db_create.py
@@ -1,5 +1,5 @@
-from app import db
-from models import BlogPost
+from project import db
+#from project.models import BlogPost
 
 #create the database and the db tables
 #creating the db based on the models.py file
@@ -7,9 +7,9 @@ db.create_all()
 
 
 #insert
-db.session.add(BlogPost("Good", "I\'m good."))
-db.session.add(BlogPost("Well", "I\'m well."))
-db.session.add(BlogPost("Flask", "discoverflask.com"))
-db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
+#db.session.add(BlogPost("Good", "I\'m good."))
+#db.session.add(BlogPost("Well", "I\'m well."))
+#db.session.add(BlogPost("Flask", "discoverflask.com"))
+#db.session.add(BlogPost("postgres", "we setup a local postgres instance"))
 #commit the changes
 db.session.commit()
